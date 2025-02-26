3991ff2f75f8b02ea88b1e1207b23d272e2c9d3c
IvanR <ivan.ramirezf@gmail.com>
2025-02-26 23:31:13 +1000

    Managing form submit with Router Action
    - Created an action in CreateOrder component
    - The action is then plugged it into the Route /order/new

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,6 +4,8 @@
 //     str
 //   );
 
+import { Form } from "react-router-dom";
+
 // const fakeCart = [
 //   {
 //     pizzaId: 12,
@@ -36,7 +38,8 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Lets go!</h2>
 
-      <form>
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -70,9 +73,17 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log("data", data);
+
+  return null;
 }
 
 export default CreateOrder;
